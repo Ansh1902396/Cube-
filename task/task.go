c4c6dec58ca580241d9f62802d51652d04eca706
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2024-03-02 20:59:49 +0530

    written the Run function Partially

--- task/task.go
+++ task/task.go
@@ -1,13 +1,45 @@
 package task
 
 import (
+	"context"
+	"io"
+	"log"
+	"os"
 	"time"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
 )
 
 type State int
+
+type Docker struct {
+	Client      *client.Client
+	Config      Config
+	ContainerId string
+}
+
+type DockerResult struct {
+	Error       error
+	Action      string
+	ContainerId string
+	Result      string
+}
+
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	Cmd           []string
+	Image         string
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
+}
 
 type Task struct {
 	ID            uuid.UUID
@@ -39,3 +71,17 @@ const (
 	Completed
 	Failed
 )
+
+// The Run Function
+func (d *Docker) Run() DockerResult {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(ctx, d.Config.Image, types.ImagePullOptions{})
+	if err != nil {
+		log.Printf("Error pulling image: %v\n", d.Config)
+		return DockerResult{Error: err}
+	}
+
+	io.Copy(os.Stdout, reader)
+}
+
+func (cli *Client) ContainerCreate(ctx context.Context, config *container.Config, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig, platform *specs.Platform, containerName string)
